d256221e4927454fef7bd19b061e0188e3de8b84
brichtom <56296772+brichtom@users.noreply.github.com>
2020-02-18 12:13:17 +0100

    Add files via upload

---
+++ build/precache-manifest.ac000b5e1e0141b71fc4d84e8e3c978f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77498b4a834d5605cf57ef445f0cae9b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b0ac929b60f03ad45d8",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f189ab09e82ab7e77ce0",
+    "url": "./static/js/2.cbe3046e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.cbe3046e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b0ac929b60f03ad45d8",
+    "url": "./static/js/main.1ba500b9.chunk.js"
+  },
+  {
+    "revision": "d8d6dbe67730a0f92533",
+    "url": "./static/js/runtime-main.f13432af.js"
+  }
+]);
